aa843ac9f9760699735053211c246892337b3bab
ryo_of_the_ryo <ryomasumura1201@gmail.com>
2023-12-26 09:05:05 +0900

    wip

--- server.go
+++ server.go
@@ -3,20 +3,27 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"reflect"
+	"runtime"
 )
 
-type MyHandler struct {}
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello!")
+}
 
-func (h *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World!")
+func log(h http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		name := runtime.FuncForPC(reflect.ValueOf(h).Pointer()).Name()
+		fmt.Println("Handler function called - " + name)
+		h(w, r)
+	}
 }
 
 
 func main() {
-	handler := MyHandler{}
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
-		Handler: &handler,
 	}
+	http.HandleFunc("/hello", log(hello))
 	server.ListenAndServe()
 }
