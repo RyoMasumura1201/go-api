c795e130ae790983f9208113ada01e8e6851f973
ryo_of_the_ryo <ryomasumura1201@gmail.com>
2023-12-27 23:13:01 +0900

    wip

--- server.go
+++ server.go
@@ -3,27 +3,25 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"reflect"
-	"runtime"
 )
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello!")
+func headers(w http.ResponseWriter, r *http.Request) {
+	h := r.Header["Accept-Encoding"]
+	fmt.Fprintln(w, h)
 }
 
-func log(h http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		name := runtime.FuncForPC(reflect.ValueOf(h).Pointer()).Name()
-		fmt.Println("Handler function called - " + name)
-		h(w, r)
-	}
+func body(w http.ResponseWriter, r *http.Request){
+	len := r.ContentLength
+	body := make([]byte, len)
+	r.Body.Read(body)
+	fmt.Fprintln(w, string(body))
 }
-
 
 func main() {
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
 	}
-	http.HandleFunc("/hello", log(hello))
+	http.HandleFunc("/headers", headers)
+	http.HandleFunc("/body", body)
 	server.ListenAndServe()
 }
